3ac5ce9c98f1b49a1a0e0b191fab245102b05110
AmalieKoder <amlie.koder@gmail.com>
2023-10-31 13:22:01 +0100

    script

--- scrpt/themeToggle.js
+++ scrpt/themeToggle.js
@@ -1,3 +1,6 @@
+// Lager en funksjon til en knapp som kan shifte bakgrunnsfargen
+// Og exporter til script.js
+
 export function themeToggled() {
     const body = document.querySelector("body");
     body.classList.toggle("darkMode");
