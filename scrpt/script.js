d62dbafad73f55fbfd04fd241a1107e52c071c13
AmalieKoder <amlie.koder@gmail.com>
2023-11-01 13:12:21 +0100

    style

--- scrpt/script.js
+++ scrpt/script.js
@@ -5,9 +5,23 @@ import { themeToggled } from "./themeToggle.js";
 // Henter fra HTML til JavaScript
 
 const button = document.querySelector("button");
+const navbar = document.querySelector(".ul");
 
 // Når denne knappen hører et klik skal (themeToggled) shifte backgrunnsfargen
 
 button.addEventListener("click", themeToggled);
+
+let navOpen = false;
+
+function toggleNav() {
+    if (navOpen === false) {
+        navbar.classList = "showNav";
+        navOpen = true;
+    }
+    else {
+        navbar.classList = "ul";
+        navOpen = false;
+    }
+}
 
 
