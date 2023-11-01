06a32458fb70ab8c60ffac0fe307579c013765ce
AmalieKoder <amlie.koder@gmail.com>
2023-11-01 13:25:31 +0100

    style

--- scrpt/hamburgerMeny.js
+++ scrpt/hamburgerMeny.js
@@ -1,6 +1,14 @@
+// Henter fra HTML til JavaScript
+
 const navbar = document.querySelector(".ul");
 
+// (let) sjekker navOpen om den er false
+
 let navOpen = false;
+
+// Lager en funksjon til en hamburger knapp
+// den dykker opp på telefon
+// Henter også fra navn fra CSS
 
 function toggleNav() {
     if (navOpen === false) {
