3c4dcb038be797183b3358e533ee610ceb33482b
AmalieKoder <amlie.koder@gmail.com>
2023-11-13 11:25:16 +0100

    html

--- scrpt/script.js
+++ scrpt/script.js
@@ -4,7 +4,7 @@ import { themeToggled } from "./themeToggle.js";
 
 // Henter fra HTML til JavaScript
 
-const button = document.querySelector("button");
+const button = document.querySelector("p");
 
 
 // Når denne knappen hører et klik skal (themeToggled) shifte backgrunnsfargen
