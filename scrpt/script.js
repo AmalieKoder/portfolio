8067a3a26111c60c6867da8a056a27404fbccd00
AmalieKoder <amlie.koder@gmail.com>
2023-11-14 14:09:07 +0100

    script

--- scrpt/script.js
+++ scrpt/script.js
@@ -13,7 +13,15 @@ const button = document.querySelector(".img");
 
 button.addEventListener("click", themeToggled);
 
+var colorShift = document.querySelector("h1");
 
+colorShift.onclick = function () {
+    var red = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+
+    this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
+};
 
 
 
