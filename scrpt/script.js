c912df3eecde2437fd7892fb2aad5b7840ec9bc0
AmalieKoder <amlie.koder@gmail.com>
2023-11-15 11:00:33 +0100

    script

--- scrpt/script.js
+++ scrpt/script.js
@@ -6,7 +6,7 @@ import { themeToggled } from "./themeToggle.js";
 // Henter fra HTML til JavaScript
 
 const button = document.querySelector(".img");
-const colorShift = document.querySelector("h1");
+
 
 
 
@@ -14,16 +14,6 @@ const colorShift = document.querySelector("h1");
 
 button.addEventListener("click", themeToggled);
 
-
-
-// Når denne function hører et click skal (h1) shifte en random fargen
-colorShift.onclick = function () {
-    var red = Math.floor(Math.random() * 256);
-    var blue = Math.floor(Math.random() * 256);
-    var green = Math.floor(Math.random() * 256);
-
-    this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
-};
 
 
 
